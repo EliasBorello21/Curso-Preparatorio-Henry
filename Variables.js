68c28cb019ced54b5063e22b9279cb62fc87544d
eliasborello21 <eliasborello@gmail.com>
2023-06-17 03:58:29 -0300

    actualizado

---
+++ Variables.js
@@ -0,0 +1,28 @@
+//String
+
+var bootcamp = "Henry";
+
+//Numeros
+
+var numeroentero = 17;
+
+var numeronegativo = -4;
+
+var numerodecimal = 11.75;
+
+//Booleanos
+
+var estoyentendiendo = true;
+
+var estoyaburrido = false;
+
+//Undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null
+
+var sinnada = null;
+ewqee
