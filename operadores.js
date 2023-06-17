8d7b435e5ef1d20e50b17fc3829f91a16e3e29d7
eliasborello21 <eliasborello@gmail.com>
2023-06-17 04:15:07 -0300

    operadores

---
+++ operadores.js
@@ -0,0 +1,22 @@
+//Suma
+console.log(5 + 2);
+
+//Resta
+console.log(5 - 2);
+
+//Division
+console.log(30 / 9);
+
+//Multiplicacion
+console.log(5 * 5);
+
+//Resto o Modulo
+console.log(95 % 4);
+
+//Potenciacion
+console.log(5 ** 2);
+
+var nombre = "Elias";
+var saludo = "Hola " + nombre;
+
+console.log(saludo);
