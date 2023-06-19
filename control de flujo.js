20207a0f8219cb6853e1ccd32fef8613d6d3bb6c
eliasborello21 <eliasborello@gmail.com>
2023-06-19 03:57:27 -0300

    control de flujo

---
+++ control de flujo.js
@@ -0,0 +1,21 @@
+function viajar(destino) {
+    if (destino === "Brasil") {
+        console.log("gire a la IZQUIERDA");
+    } else if (destino === "Argentina") {
+        console.log("gire a la DERECHA");
+    } else {
+        console.log("Nos perdimos");
+    }
+}
+
+viajar("wetfewf");
+
+function puede_manejar (edad) {
+    if (edad >= 18) {
+        console.log("puede manejar");
+    } else {
+        console.log("no puede manejar");
+    }
+}
+
+puede_manejar(13);
